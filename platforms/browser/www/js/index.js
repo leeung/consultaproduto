0ec9d5e0f08bd23cea41f4a98ec02af9c74d7c33
leeung <leeungalves@gmil.com>
2021-01-19 17:01:36 -0300

    commit inicial

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,42 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    bindEvents: function () {
+        //document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('click', this.consultaProduto, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
     },
+
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent2: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -45,5 +26,41 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    consultaProduto: function (id) {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                /*alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);*/
+                app.getEPI(result.text);
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    },
+
+    getEPI: function (cod) {
+        $.ajax({
+            url: "http://192.168.0.200/srvbuscapreco/index.php/Consulta/consultaproduto/" + cod + "/010115/001",
+            type: "POST",
+            dataType: "json"
+        }).done(function (resposta) {
+            $('#img').html('<img src="http://192.168.0.200/srvbuscapreco/assets/imagens_produtos/'+resposta.result.IMG+'">');
+            $('#barra').html(resposta.result.EAN);
+            $('#codigo').html(resposta.result.CODIGO);
+            $('#descricao').html(resposta.result.DESCRICAO);
+            $('#varejo').html("VAREJO: "+resposta.result.VAREJO);
+            $('#atacado').html("ATACADO: "+resposta.result.ATACADO);
+        }).fail(function (jqXHR, textStatus) {
+            console.log('falhou')
+        }).always(function () {
+            console.log('concluiu');
+        });
+
     }
+
 };
